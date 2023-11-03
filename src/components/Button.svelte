e76a3ec6d8403fa3e07743b58be4f8bde3ff4deb
Maeva Ngoban <maevangoban@gmail.com>
2023-11-03 13:42:43 +0100

    speed

---
+++ src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script>
+	
+	let buttonProps = {
+	class:[$$restProps.class]
+	}
+
+    export let onclick 
+</script>
+	<button on:click = {onclick}>
+
+			<slot/>
+	</button>
+
+<style>
+	.primary{
+		color:green;
+	}
+	.danger {
+		color:red;
+	}
+	.sm {
+		font-size:1em;
+		padding:0.1em;
+	}
+	.lg {
+		font-size:2em
+	}
+</style>
